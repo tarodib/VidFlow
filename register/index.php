d33da44a678c0c5a7a8c5f869bbc5e03cf581910
B.Barna <84066062+barnatester@users.noreply.github.com>
2024-10-22 11:27:56 +0200

    Adding register files

---
+++ register/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="registerstyle.css"/>
+    <title>Regisztráció - VidFlow</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;500;700&display=swap" rel="stylesheet"> <!-- Corrected this line -->
+    <link rel="stylesheet" href="registerstyle.css"/>
+</head>
+<body>
+    <img src="/vidflow/vidflow_official_logo.png" alt="" class="logo">
+    <div class="registration">
+        <h2 class="registration_title" style="font-family: 'Josefin Sans';">REGISZTRÁCIÓ</h2>
+        <input type="text" class="field" id="name" placeholder="Name"/><br><br>
+        <input type="text" class="field" id="email" placeholder="Email"><br><br>
+        <input type="text" class="field" id="username" placeholder="Username"><br><br>
+        <input type="password" class="field" id="password" placeholder="Password"><br><br>
+        <input type="password" class="field" id="passwordconfirm" placeholder="Confirm Password"><br><br>
+        <input type="submit" class="register" value="Register">
+        <h1><?php print "alskfhsdukfh" ?></h1>
+    </div>
+</body>
+</html>
