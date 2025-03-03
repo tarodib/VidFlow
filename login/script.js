95a74c5f3885bacc1d3140b07bc354afd9310b20
B.Barna <84066062+barnatester@users.noreply.github.com>
2025-03-03 20:25:47 +0100

    First working version Barna's part
    These files are the files that are Barna's part for the project

---
+++ login/script.js
@@ -0,0 +1,13 @@
+function openGoogleLogin() {
+
+}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); 
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+function openBarnatechLogin() {
+    
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+function goHomePage(){
+    location.href = "/";
+}
+
+function switchLogoutRed(){
+    document.getElementById("logouticon").src = "pageelements/logout_red.png";
+}
+
+function switchLogoutDefault(){
+    document.getElementById("logouticon").src = "pageelements/logout.png";
+}
